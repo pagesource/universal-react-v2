0500951035a8fa100f2eca85db7a133f0fbf28fa
Hitesh Parashar <hitparas@publicisgroupe.net>
2020-05-09 13:20:36 +0530

    fix: accommodate additional tempates added in the main project

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const path = require('path');
 const chalk = require('chalk');
 const inquirer = require('inquirer');
 const { createNpmDependenciesArray, mergeJsons } = require('./utils/jsonHelper');
+const { createAppQuestions } = require('./utils/questions');
 const {
   appTemplateFileExclusions,
-  createAppQuestions,
   universalReactStampData
 } = require('./utils/constants');
 const { createDir, copyDir, writeJsonFile } = require('./utils/fileDirOps');
@@ -15,24 +15,27 @@ const { installPackages } = require('./utils/install');
 
 const templatesPath = path.join(__dirname, 'templates');
 const baseTemplatePath = path.join(templatesPath, 'base');
-const staticTemplatePath = path.join(templatesPath, 'static');
-const ssrTemplatePath = path.join(templatesPath, 'ssr');
+let templatePath = '';
 let projectDir = '';
 const cwd = process.cwd();
 
 const stampFileName = 'universal-react-stamp.json';
 
-const run = (appType, appName) => {
+const createProjectDirectory = (appName) => {
   projectDir = path.join(cwd, appName);
   createDir(projectDir);
+};
 
+const copyBaseDirectory = () => {
   copyDir(baseTemplatePath, projectDir, appTemplateFileExclusions);
+};
 
-  const templatePath = appType === 'static' ? staticTemplatePath : ssrTemplatePath;
+const copyTemplateDirectory = (appType) => {
+  templatePath = path.join(templatesPath, appType);
   copyDir(templatePath, projectDir, appTemplateFileExclusions);
+};
 
-  console.info(chalk.green('project created successfully'));
-
+const mergeAndCopyPackageFile = (appName) => {
   const basePackage = require(path.join(baseTemplatePath, 'package.json'));
   const appPackage = require(path.join(templatePath, 'package.json'));
   let packageFile = mergeJsons(basePackage, appPackage);
@@ -45,20 +48,44 @@ const run = (appType, appName) => {
     console.error(chalk.red('error copying package.json file'));
     throw e;
   }
+};
 
+const createStampFile = (appType) => {
+  const universalReactPackageFile = require(path.join(__dirname, 'package.json'));
+  const json = mergeJsons(universalReactStampData, {
+    name: universalReactPackageFile.name,
+    version: universalReactPackageFile.version,
+    type: appType
+  });
   try {
-    writeJsonFile(path.join(projectDir, stampFileName), universalReactStampData);
+    writeJsonFile(path.join(projectDir, stampFileName), json);
   } catch (e) {
     console.error('error creating stamp file');
   }
+};
 
+const installDependencies = () => {
   console.info(chalk.yellow('installing packages'));
   const depArr = createNpmDependenciesArray(path.join(projectDir, 'package.json'));
   installPackages(projectDir, depArr);
 };
 
+const initialize = (appType, appName) => {
+  createProjectDirectory(appName);
+  copyBaseDirectory();
+  copyTemplateDirectory(appType);
+  console.info(chalk.green('project created successfully'));
+
+  mergeAndCopyPackageFile(appName);
+  createStampFile(appType);
+
+  installDependencies();
+};
+
+// initialize('ssr-custom', 'appName');
+
 inquirer.prompt(createAppQuestions).then((answers) => {
   console.info(JSON.stringify(answers, null, '  '));
 
-  run(answers.appType, answers.appName);
+  initialize(answers.appType, answers.appName);
 });
