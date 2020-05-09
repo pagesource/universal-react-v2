d6c0cbea5dc6abbfae873d90375d2317e9d1dfe7
Achal Jain <achal@MBP.local>
2020-05-09 16:57:14 +0530

    fix: add apollo-link-error

--- templates/base/graphQl/ApolloClient.js
+++ templates/base/graphQl/ApolloClient.js
@@ -1,20 +1,34 @@
 import ApolloClient, { HttpLink, InMemoryCache } from 'apollo-boost';
+import { onError } from 'apollo-link-error';
 import { graphQLApiUri } from '../config/api';
 
 const cache = new InMemoryCache();
 
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
 /**
  * GraphQLClient : Returns instance of apollo client.
  * @param {*} uri [URL of service ]
- * @param {*} headers [headers to be pased to service. like security token]
+ * @param {*} headers [headers to be passed to service. like security token]
  */
 const GraphQLClient = (uri = graphQLApiUri, headers = {}) =>
   new ApolloClient({
     cache,
-    link: new HttpLink({
-      uri,
-      headers
-    })
+    link: ApolloLink.from([
+      errorLink,
+      new HttpLink({
+        uri,
+        headers
+      })
+    ])
   });
 
 export default GraphQLClient;
