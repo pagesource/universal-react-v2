9c92a7e778b865fb20aee7e6782aaeca85fccd36
Ankush Saini <anksaini1@publicisgroupe.net>
2020-05-09 19:47:33 +0530

    feat(router-link-config): add link atom

--- templates/base/next.config.js
+++ templates/base/next.config.js
@@ -1,7 +1,9 @@
+const isAssetPrefix = process.env.BASE_PATH || '';
+
 module.exports = {
   distDir: 'build',
-  assetPrefix: process.env.BASE_PATH || '',
-  publicRuntimeConfig: {
-    basePath: process.env.BASE_PATH || '',
+  assetPrefix: isAssetPrefix,
+  env: {
+    BASE_PATH: isAssetPrefix
   }
-}
+};
--- templates/base/src/routes/index.js
+++ templates/base/src/routes/index.js
@@ -1,24 +1,9 @@
-import React from 'react'
 import Router from 'next/router';
-import getConfig from 'next/config';
 
-const {
-  publicRuntimeConfig
-} = getConfig();
+export const BASE_PATH = process.env.BASE_PATH || '';
 
-export const BASE_PATH = publicRuntimeConfig.basePath;
-
-export const NextRouterWrapper = ({
-  pathName,
-  as,
-  options = {}
-}) => {
+export const NextRouterWrapper = ({ pathName, as, options = {} }) => {
   Router.push(pathName, `${BASE_PATH}${as || pathName}`, options);
 };
 
-export const getBasePath = route =>
-  `${BASE_PATH}${route}`
-
-export default {
-  home: '/'
-};
+export const getBasePath = (route) => `${BASE_PATH}${route}`;
