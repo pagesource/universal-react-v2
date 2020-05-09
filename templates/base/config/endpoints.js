79d2100799ce9c6129369b229c21ae34ba9c6686
vinodloha <vinodloha@gmail.com>
2020-05-09 16:33:50 +0530

    feat(fetch): restructure fetch wrap
    - Defined a endpoint Utility
    - Defined structure to endpoints
    - updated tests

--- templates/base/config/endpoints.js
+++ templates/base/config/endpoints.js
@@ -1,29 +1,16 @@
-
 /** list of apis to be called */
-const endpoints = {
-  getMockJson: '/todos/1',
-}
+export const endpoints = {
+  content: {
+    home: 'home/1'
+  },
+  apiGee: {
+    todo: 'todos/1'
+  }
+};
 
 /** various domains entry
  * 1. contentDomain - for content type like labels, api call to graphql, etc
  * 2. dataDomain - for data type, like api call for user data
-*/
-const contentDomain = '';
-const dataDomain = 'https://jsonplaceholder.typicode.com';
-
-
-/** getContentServiceUrl - generates content service url to be passed to fetch api
- * (key, isMock) - > key - get url from endpoints ;  isMock - serve data from public folder
-*/
-export const getContentServiceUrl = (key, isMock = false) => {
-  const url = endpoints[key];
-  return isMock ? url : `${contentDomain}url`;
-}
-
-/** getDataServiceUrl - generates data service url to be passed to fetch api
- * (key, isMock) - > key - get url from endpoints ;  isMock - serve data from public folder
-*/
-export const getDataServiceUrl = (key, isMock = false) => {
-  const url = endpoints[key];
-  return isMock ? `${url}.json` : `${dataDomain}${url}`; // mock file can be changed as preference
-}
+ */
+export const contentDomain = 'https://jsonplaceholder.typicode.com/';
+export const apiGeeDomain = 'https://jsonplaceholder.typicode.com/';
--- templates/base/src/utils/serviceUtils/fetch.js
+++ templates/base/src/utils/serviceUtils/fetch.js
@@ -3,35 +3,40 @@ import fetch from 'isomorphic-unfetch';
 const defaultHeaders = {
   headers: {
     'Content-Type': 'application/json'
-    // 'Content-Type': 'application/x-www-form-urlencoded',
-  },
-}
+  }
+};
 
 /**
  *
  * @param {*} response  - response from the fetch api
  * checks whether the response has a status ok or else returns error
  */
-const checkStatus = response => {
+const checkStatus = (response) => {
   console.log(response);
   if (response.ok) {
     return response;
-  }
-  else {
+  } else {
     let error = new Error({ status: response.statusText, errMessage: response.json() });
     console.log('error=====>>>>', error);
     error.response = response;
     return Promise.reject(error);
   }
-}
+};
 
 /**
  *
  * @param {*} url - url returned from endpoints getContentServiceUrl  || getDataServiceUrl
  * @param {*} fetchOptions - other options like body or headers
  */
-async function executeAPICall(url, fetchOptions) {
-  const { headers, isCredentialsForCrossOrigin, cache, redirect, referrerPolicy, body = null } = fetchOptions || {};
+async function fetchWrapper(url, fetchOptions) {
+  const {
+    headers,
+    isCredentialsForCrossOrigin,
+    cache,
+    redirect,
+    referrerPolicy,
+    body = null
+  } = fetchOptions || {};
   const payload = {
     method: 'GET',
     headers: { ...defaultHeaders, ...headers },
@@ -40,13 +45,17 @@ async function executeAPICall(url, fetchOptions) {
     cache: cache || 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
     credentials: isCredentialsForCrossOrigin ? 'include' : 'same-origin', // include, *same-origin, omit -> to pass the credentials for cross-origin use include
     redirect: redirect || 'follow', // manual, *follow, error
-    referrerPolicy: referrerPolicy || 'no-referrer',
-  }
+    referrerPolicy: referrerPolicy || 'no-referrer'
+  };
   return await fetch(url, {
     ...payload
-  }).then(checkStatus)
-    .then(res => res.json())
-    .catch(err => { console.log('err=====>>>>', err); return err; });
+  })
+    .then(checkStatus)
+    .then((res) => res.json())
+    .catch((err) => {
+      console.log('err=====>>>>', err);
+      return err;
+    });
 }
 
-export default executeAPICall;
+export default fetchWrapper;
