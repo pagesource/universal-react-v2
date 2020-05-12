c84d282e307d4af37cd9ed921675573fc0b0e182
Rishabh Agrawal <rishabh.agrawal@publicissapient.com>
2020-05-12 15:13:25 +0530

    feat: config react hooks testing + create a dummy react hook component with test file

--- templates/base/jest.config.js
+++ templates/base/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  testMatch: ['<rootDir>/**/**/__tests__/*(*.)@(spec|test).[jt]s?(x)'],
+  testMatch: ['<rootDir>/**/**/__tests__/*(*.)@(spec|test).[jt]s?(x)',
+    '<rootDir>/**/**/**/tests/*(*.)@(spec|test).[jt]s?(x)'],
   moduleDirectories: ['node_modules'],
   testPathIgnorePatterns: ['<rootDir>/dist', '<rootDir>/out/'],
   clearMocks: true,
