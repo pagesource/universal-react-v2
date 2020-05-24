785cc21bdca6aff77cb07a0ca8926d95b0a53228
Amit Chawla <amit.chawla2@sapient.com>
2020-05-24 11:37:04 +0530

    chore:Basic PWA integration added

--- templates/base/next.config.js
+++ templates/base/next.config.js
@@ -1,13 +1,30 @@
+require('dotenv').config();
+const withPWA = require('next-pwa');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 });
 
 const isAssetPrefix = process.env.BASE_PATH || '';
 
+/**
+ * This section adds the basic PWA configuration into the next config
+ * It derives from an env variable defined in .env file. Default it is enabled,
+ * To disable it you need to specify the PWA_DISABLED=true in .env file
+ * 
+ * Further PWA configuration visit https://www.npmjs.com/package/next-pwa
+ */
+const nextConfig = withPWA({
+  pwa: {
+    disable: process.env.PWA_DISABLED === 'true',
+    dest: 'public'
+  }
+})
+
 module.exports = withBundleAnalyzer({
   distDir: 'build',
   assetPrefix: isAssetPrefix,
   env: {
     BASE_PATH: isAssetPrefix
-  }
+  },
+  ...nextConfig
 });
--- templates/base/src/pages/_document.js
+++ templates/base/src/pages/_document.js
@@ -10,7 +10,19 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta name='application-name' content='Universal React V2' />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+          <meta name='apple-mobile-web-app-title' content='Universal React V2' />
+          <meta name='description' content='Change description with your own' />
+          <meta name='mobile-web-app-capable' content='yes' />
+          <meta name='msapplication-TileColor' content='#2B5797' />
+          <meta name='msapplication-tap-highlight' content='no' />
+          <meta name='theme-color' content='#000000' />
+          <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
+          <link rel='manifest' href='/public/manifest.json' />
+        </Head>
         <body>
           <Main />
           <NextScript />
