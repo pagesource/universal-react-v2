fccba47e15ea1d12d17ef9ceae289397543bdab5
KhanJr <khajr@gmail.com>
2021-09-28 19:03:03 +0530

    feat: lint on commitmsg & added sementic-release support

---
+++ templates/base/.commitlintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  extends: ['@commitlint/config-angular'],
+  rules: {
+    'scope-case': [
+      2,
+      'always',
+      [
+        'lower-case', // default
+        'upper-case', // UPPERCASE
+        'camel-case', // camelCase
+        'kebab-case', // kebab-case
+        'pascal-case', // PascalCase
+        'sentence-case', // Sentence case
+        'snake-case', // snake_case
+        'start-case' // Start Case
+      ]
+    ],
+    'subject-case': [
+      2,
+      'always',
+      [
+        'lower-case', // default
+        'upper-case', // UPPERCASE
+        'camel-case', // camelCase
+        'kebab-case', // kebab-case
+        'pascal-case', // PascalCase
+        'sentence-case', // Sentence case
+        'snake-case', // snake_case
+        'start-case' // Start Case
+      ]
+    ]
+  }
+};
