23feb01c9342b13005a6a19c59b1d7ba87ade8d9
Haeckerzz <sharad.rai.student@gmail.com>
2021-09-30 10:04:27 +0530

    fix(Storybook) : changes 1) removed addon.js and added addons to main.js

--- templates/base/storybook/.storybook/addons.js
+++
@@ -1,5 +0,0 @@
-import '@storybook/addon-actions/register';
-import '@storybook/addon-links/register';
-import '@storybook/addon-viewport/register';
-import '@storybook/addon-docs/register';
-import '@storybook/addon-a11y/register';
--- templates/base/storybook/.storybook/main.js
+++ templates/base/storybook/.storybook/main.js
@@ -1,4 +1,13 @@
 module.exports = {
-  stories: ['../../src/components/**/**/*.(stories|story).js'],
-  addons: ['@storybook/addon-docs'],
+  stories: ["../../src/components/**/**/*.(stories|story).js"],
+  addons: [
+    "@storybook/addon-a11y",
+    "@storybook/addon-actions",
+    "@storybook/addon-docs",
+    "@storybook-addon-designs",
+    "@storybook/addon-knobs",
+    "@storybook/addon-links",
+    "@storybook-design-token",
+    "@storybook/addon-viewport"
+  ],
 };
