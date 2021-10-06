412253c8e089586feacef82bf276b8118d27ed31
suhas-gopinath <emailofsuhas@gmail.com>
2021-10-06 14:55:40 +0530

    fix: minor bug fx

--- templates/base/src/localization/useFetchWithLocale.js
+++ templates/base/src/localization/useFetchWithLocale.js
@@ -1,77 +1,77 @@
 import { useState, useEffect } from 'react';
 
-const defaultHeaders = {
-  headers: {
-    'Content-Type': 'application/json'
-  }
-};
-
-const checkStatus = (response) => {
-  if (response.ok) {
-    return response;
-  } else {
-    let error = new Error({ statusText: response.statusText, response: response.json() });
-    return Promise.reject(error);
-  }
-};
-
-async function fetchWrapper(url, fetchOptions) {
-  const {
-    headers,
-    isCredentialsForCrossOrigin,
-    cache,
-    redirect,
-    referrerPolicy,
-    body = null
-  } = fetchOptions || {};
-  const payload = {
-    method: 'GET',
-    headers: { ...defaultHeaders, ...headers },
-    body,
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: cache || 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: isCredentialsForCrossOrigin ? 'include' : 'same-origin', // include, *same-origin, omit -> to pass the credentials for cross-origin use include
-    redirect: redirect || 'follow', // manual, *follow, error
-    referrerPolicy: referrerPolicy || 'no-referrer'
-  };
-  return await fetch(url, {
-    ...payload
-  })
-    .then(checkStatus)
-    .then((res) => {
-      res.json()
-      setData(res.json())
-      setLoading(false)
-    })
-    .catch((err) => {
-      setError(err)
-      Logger.error({
-        message: 'failed to fetch',
-        error: {
-          code: err.statusText,
-          errMessage: err.response,
-          operationName: url
-        },
-        service: {
-          name: '',
-          path: url,
-        }
-      });
-      return err;
-    });
-}
-
-const [loading, setLoading] = useState(true);
-const [data, setData] = useState();
-const [error, setError] = useState();
-const languageSessionStorageKey = "language"
-
 export default function useFetchWithLocale(url, fetchOptions){
 
-  const language = JSON.parse(localStorage.getItem(languageKey))
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState();
+  const [error, setError] = useState();
+  const languageSessionStorageKey = "language"
+
+  const language = JSON.parse(localStorage.getItem(languageSessionStorageKey))
 
   //Logic for handling multi locale requests
   // url = "/"+language+"/"+url
+
+  const defaultHeaders = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+  
+  const checkStatus = (response) => {
+    if (response.ok) {
+      return response;
+    } else {
+      let error = new Error({ statusText: response.statusText, response: response.json() });
+      return Promise.reject(error);
+    }
+  };
+
+  async function fetchWrapper(url, fetchOptions) {
+    const {
+      headers,
+      isCredentialsForCrossOrigin,
+      cache,
+      redirect,
+      referrerPolicy,
+      body = null
+    } = fetchOptions || {};
+    const payload = {
+      method: 'GET',
+      headers: { ...defaultHeaders, ...headers },
+      body,
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: cache || 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: isCredentialsForCrossOrigin ? 'include' : 'same-origin', // include, *same-origin, omit -> to pass the credentials for cross-origin use include
+      redirect: redirect || 'follow', // manual, *follow, error
+      referrerPolicy: referrerPolicy || 'no-referrer'
+    };
+    return await fetch(url, {
+      ...payload
+    })
+      .then(checkStatus)
+      .then((res) => {
+        res.json()
+        setData(res.json())
+        setLoading(false)
+      })
+      .catch((err) => {
+        setError(err)
+        Logger.error({
+          message: 'failed to fetch',
+          error: {
+            code: err.statusText,
+            errMessage: err.response,
+            operationName: url
+          },
+          service: {
+            name: '',
+            path: url,
+          }
+        });
+        return err;
+      });
+  }
 
   useEffect(() => {
     fetchWrapper(url,fetchOptions);
