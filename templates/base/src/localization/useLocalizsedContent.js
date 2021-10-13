b97ac4bad587c71ea1dcb0c9caf39c9609dbe026
suhas-gopinath <emailofsuhas@gmail.com>
2021-10-13 16:37:29 +0530

    fix: imported fetchWrapper instead of redifining it

--- templates/base/src/localization/useLocalizsedContent.js
+++ templates/base/src/localization/useLocalizsedContent.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import fetchWrapper from '../utils/serviceUtils/fetch';
 
 const languageStorageKey = "language"
 
@@ -10,91 +11,26 @@ const setLanguageToLocalStorage = (language) => {
   localStorage.setItem(languageKey, language);
 }
 
-const getLanguageFromSessionStorage = () => {
-  return sessionStorage.getItem(languageKey);
-}
-
-const setLanguageToSessionStorage = (language) => {
-  sessionStorage.setItem(languageKey, language);
-}
-
-export default function useLocalizsedContent(url, fetchOptions){
+export default function useLocalisedContent(url, fetchOptions){
 
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState();
-  const [error, setError] = useState();
 
   //Logic for handling multi locale requests
-  
-  // const language = getLanguageFromLocalStorage();
-  // const language = getLanguageFromSessionStorage(); 
+  const language = getLanguageFromLocalStorage();
 
-  // url = "/"+language+"/"+url
-
-  const defaultHeaders = {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
-  
-  const checkStatus = (response) => {
-    if (response.ok) {
-      return response;
-    } else {
-      let error = new Error({ statusText: response.statusText, response: response.json() });
-      return Promise.reject(error);
-    }
-  };
-
-  async function fetchWrapper(url, fetchOptions) {
-    const {
-      headers,
-      isCredentialsForCrossOrigin,
-      cache,
-      redirect,
-      referrerPolicy,
-      body = null
-    } = fetchOptions || {};
-    const payload = {
-      method: 'GET',
-      headers: { ...defaultHeaders, ...headers },
-      body,
-      mode: 'cors', // no-cors, *cors, same-origin
-      cache: cache || 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: isCredentialsForCrossOrigin ? 'include' : 'same-origin', // include, *same-origin, omit -> to pass the credentials for cross-origin use include
-      redirect: redirect || 'follow', // manual, *follow, error
-      referrerPolicy: referrerPolicy || 'no-referrer'
-    };
-    return await fetch(url, {
-      ...payload
-    })
-      .then(checkStatus)
-      .then((res) => {
-        res.json()
-        setData(res.json())
-        setLoading(false)
-      })
-      .catch((err) => {
-        setError(err)
-        Logger.error({
-          message: 'failed to fetch',
-          error: {
-            code: err.statusText,
-            errMessage: err.response,
-            operationName: url
-          },
-          service: {
-            name: '',
-            path: url,
-          }
-        });
-        return err;
-      });
+  if(!language){
+    //default language 
+    language="en-us";
   }
 
+  url = "/"+language+"/"+url
+
   useEffect(() => {
-    fetchWrapper(url,fetchOptions);
+    setLoading(true);
+    setData(fetchWrapper(url,fetchOptions));
+    setLoading(false);
   }, [url])
 
-return [loading, data, error]
+return [loading, data]
 }
