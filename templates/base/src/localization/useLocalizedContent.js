f4ef86ca5d2baa281f86fe1274276d35df8e8fb4
suhas-gopinath <emailofsuhas@gmail.com>
2021-10-26 12:16:08 +0530

    fix: removed data state and added getLabel and getAllLAbels

--- templates/base/src/localization/useLocalizedContent.js
+++ templates/base/src/localization/useLocalizedContent.js
@@ -9,36 +9,36 @@ const basePath = "http://localhost:3000";
 const getLanguageFromLocalStorage = () => {
   const language = localStorage.getItem(languageStorageKey);
   //en-us is the default locale
-  return language || "en-us";
+  return language || "en_us";
 }
 
 const getCacheKey = (language,url) => {
-  url = url.replace(/-/g,"_");
-  url = url.replace(/\//g,"_");
+  url = url.replace(/[-/]/g,"_"));
   return `${language}_${url}`;
 }
 
 export default function useLocalizedContent(url, fetchOptions){
 
   const [loading, setLoading] = useState(true);
-  const [data, setData] = useState(null);
 
   //Logic for handling multi locale requests
   const language = getLanguageFromLocalStorage();
   const cacheKey = getCacheKey(language,url);
   const localizedUrl = `${basePath}/${language}/${url}`;
 
+  const getAllLabels = () => cache[cacheKey];
+  
+  const getLabel = (label) => cache[cacheKey][label];
+
   useEffect(() => {
     setLoading(true);
 
     if(!(cacheKey in cache)){
-      cache[cacheKey]=fetchWrapper(localizedUrl,fetchOptions);
+      cache[cacheKey] = fetchWrapper(localizedUrl,fetchOptions);
     }
-    
-    setData(cache[cacheKey]);
-    setLoading(false);
 
+    setLoading(false);
   }, [url])
 
-return [loading, data]
+return [loading, getLabel, getAllLabels]
 }
