2855f9d359ac47c332df1637c5e85f215dd6007f
Rakesh Kumar <rakesh.kumar1@publicissapient.com>
2022-01-27 12:35:46 +0530

    commented out the stamp file update code need to review in future

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ let microAppPath = ''; // project folder under ./apps/${appName}
 let packagesAppPath = ''; // packages folder on the same level of ./apps
 let turboRepoPackageFile = ''; // root folder package.json file path
 const cwd = process.cwd(); // current working directory
-const stampFileName = 'universal-react-stamp.json'; // TODOs: need to remove as will depricate in future commit
+// const stampFileName = 'universal-react-stamp.json'; // TODOs: need to remove as will depricate in future commit
 
 /**
  * @description : method to initialize git repositiy
@@ -195,10 +195,10 @@ const addInfoIntoPackageJson = async (appType, appName) => {
 };
 
 /**
+ * @deprecated : this method need to remove
  * @description : method to updated turbo repo package.json
  * @param {*} features : list of optional features
  * @param {*} _path : project directory path
- */
 const updateStampFile = async (features, _path = cwd) => {
   const stampFilePath = path.join(_path, stampFileName);
   if (features.length > 0) {
@@ -215,6 +215,7 @@ const updateStampFile = async (features, _path = cwd) => {
     console.info('No optional feature was applied.');
   }
 };
+ */
 
 /**
  * @description : method to install depencies of project
@@ -273,7 +274,7 @@ const initializeNewProject = async (
  */
 const updateProject = async (features) => {
   const features_found = await copyOptionalTemplates(features);
-  updateStampFile(features_found); //TODOs: need to update root packages.json with newly added optional feature
+  // updateStampFile(features_found); //TODOs: need to update root packages.json with newly added optional feature
   if (features_found.length > 0) {
     installDependencies(path.join(cwd, appConstants.PACKAGE_JSON), cwd);
   }
