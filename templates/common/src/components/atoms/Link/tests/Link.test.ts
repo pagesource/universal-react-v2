4759995452c00ebf286a3df358c8fd6327ab11f3
shubham43MP <shubhamdixit19950321@gmail.com>
2022-02-07 14:26:02 +0530

    feat: Added base TS UT for link component

---
+++ templates/common/src/components/atoms/Link/tests/Link.test.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+it('Testing the Link Router', () => {
+  expect(true).toBe(true);
+});
