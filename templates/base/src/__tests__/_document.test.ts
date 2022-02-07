17b44ff55f78f5157717b8498823cf73ff58cd5f
shubham43MP <shubhamdixit19950321@gmail.com>
2022-02-07 13:02:33 +0530

    feat: Added test cases for some files with updated TS

---
+++ templates/base/src/__tests__/_document.test.ts
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import MyDocument from '../pages/_document';
+
+describe('<MyDocument />', () => {
+  it('should render without errors', async () => {
+    expect(true).toBe(true);
+  });
+});
