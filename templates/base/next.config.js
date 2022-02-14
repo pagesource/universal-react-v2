0e0658ec7e352a020f17b7561334495df79d5ec5
Achal Jain <achal41@gmail.com>
2022-02-14 11:39:09 +0530

    fix: added next transpile

--- templates/base/next.config.js
+++ templates/base/next.config.js
@@ -2,13 +2,15 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 });
 
+const withTM = require('next-transpile-modules')(['services']);
+
 const isAssetPrefix = process.env.BASE_PATH || '';
 
-module.exports = withBundleAnalyzer({
+module.exports = withBundleAnalyzer(withTM({
   distDir: 'build',
   assetPrefix: isAssetPrefix,
   basePath: isAssetPrefix,
   env: {
     BASE_PATH: isAssetPrefix
   }
-});
+}));
