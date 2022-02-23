d03179da0db6bd06befdf208d57f8b2faca8fe1f
Vinod Kumar <vinkumar2@WKMZTFB38492.global.publicisgroupe.net>
2022-02-23 17:12:57 +0530

    A bit of code optimization

--- templates/common/src/components/atoms/Button/Button.style.ts
+++ templates/common/src/components/atoms/Button/Button.style.ts
@@ -1,12 +1,12 @@
 import styled, { css } from 'styled-components';
 
 export const AppContainer = styled.button`
-  color: ${(props) => props.theme.colors.white[0]};
+  color: ${({theme}) => theme.colors.white[0]};
   font-size: 1em;
   margin: 1em;
   padding: 0.25em 1em;
-  border: 2px solid ${(props) => props.theme.colors.black[0]};
-  background-color: ${(props) => props.theme.colors.black[0]};
+  border: 2px solid ${({theme}) => theme.colors.black[0]};
+  background-color: ${({theme}) => theme.colors.black[0]};
   border-radius: 3px;
   display: block;
 `;
