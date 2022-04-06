65dbafeb1853ac4d403a0c3f333764b72fef6e81
Apoorva2696 <apoorva2696@gmail.com>
2022-04-06 12:09:04 +0530

    cg

---
+++ new-app/apps/home/src/components/atoms/Button/Button.style.ts
@@ -0,0 +1,13 @@
+import { styled } from '@nextui-org/react';
+
+const CompRoot = styled('button', {
+  fontSize: '$3',
+  color: '$fontColor',
+  margin: '1em',
+  padding: '0.25em 1em',
+  border: '2px solid black',
+  borderRadius: '$1',
+  length: '1'
+});
+
+export default CompRoot;
